1030e41a3221c872e6622cf9e10a3ca4a1d81f08
Taiming Chen <taiming@bridgewell.com>
2019-06-22 16:02:39 +0800

    Updates

---
+++ precache-manifest.b30d6570c666d6d0f8379203af413958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbce29ae143073f9254fecba4cc8745f",
+    "url": "/roarnews_frontend/index.html"
+  },
+  {
+    "revision": "07c1bb5dddcb608581c5",
+    "url": "/roarnews_frontend/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/roarnews_frontend/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "07c1bb5dddcb608581c5",
+    "url": "/roarnews_frontend/static/js/main.9691bbcb.chunk.js"
+  },
+  {
+    "revision": "33258a5eb1008340739a",
+    "url": "/roarnews_frontend/static/js/runtime~main.36ca5e79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/roarnews_frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
